4ffada9fb12267293fb3e7500ae9be3d3381c6a0
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-08-09 11:09:33 -0500

    day41

---
+++ src/utilities/Data.java
@@ -0,0 +1,17 @@
+package utilities;
+
+public class Data {
+    public String str1,str2;
+
+    public void method1(){}
+
+    public void method2(){}
+
+
+
+    public static String str3, str4, str5;
+
+    public static void method3(){}
+
+    public static void method4(){}
+}
