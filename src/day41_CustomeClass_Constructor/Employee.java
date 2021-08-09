9270281a242c1de28e6f4de5fe8c898b6df3b6a2
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-08-09 13:56:30 -0500

    day41

--- src/day41_CustomeClass_Constructor/Employee.java
+++ src/day41_CustomeClass_Constructor/Employee.java
@@ -1,4 +1,21 @@
 package day41_CustomeClass_Constructor;
 
 public class Employee {
+
+    public  String name,jobTitle;
+    public double salary;
+
+    public Employee(String name, String jobTitle,double salary){
+        this.name=name;
+        this.jobTitle=jobTitle;
+        this.salary=salary;
+    }
+
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
---
+++ src/day41_CustomeClass_Constructor/EmployeeObjects.java
@@ -0,0 +1,16 @@
+package day41_CustomeClass_Constructor;
+
+public class EmployeeObjects {
+    public static void main(String[] args) {
+
+
+    Employee employee1 = new Employee("Sabir","SDET",110000);
+
+  Employee employee2= new Employee("Maria","SM",115000);
+
+
+        System.out.println(employee1);
+        System.out.println(employee2);
+
+    }
+}
