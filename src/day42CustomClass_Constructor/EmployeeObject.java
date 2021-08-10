aaa2088009fa93aecb211ec66655e3423c1679d5
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-08-10 14:07:57 -0500

    day41

---
+++ src/day42CustomClass_Constructor/EmployeeObject.java
@@ -0,0 +1,17 @@
+package day42CustomClass_Constructor;
+
+public class EmployeeObject {
+
+    public static void main(String[] args) {
+
+        Employee employee1=new Employee("Sabir");
+        Employee employee2 =new Employee("Zulkhumor","SDET");
+        Employee employee3 =new Employee("Maftuna","QA",1234);
+        Employee employee4 =new Employee("Ahmet","SDET", 123456,220001);
+
+        System.out.println(employee1);
+        System.out.println(employee2);
+        System.out.println(employee3);
+        System.out.println(employee4);
+    }
+}
