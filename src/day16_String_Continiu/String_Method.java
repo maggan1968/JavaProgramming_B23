6880af93cf2f988852f20301fb8cf486b1ef80e8
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-08-10 22:50:15 -0500

    day41

--- src/day16_String_Continiu/String_Method.java
+++ src/day16_String_Continiu/String_Method.java
@@ -24,7 +24,7 @@ public class String_Method {
         //      index:      0123456789
         int indexOfFirstA = sentence2.indexOf("a");
         int indexOfSecondA = sentence2.indexOf("a ");
-        int indexOfThirdA = sentence2.indexOf("a c")  ;
+        int indexOfThirdA = sentence2.indexOf("a c");
         int indexOfFourthA = sentence2.indexOf("an");
         // int indexOfFifthA = sentence2.indexOf("ag");
         int indexOfFifthA = sentence2.lastIndexOf("a");
@@ -36,7 +36,7 @@ public class String_Method {
         System.out.println("indexOfFifthA = " + indexOfFifthA);
 
         System.out.println("-----------------------------------");
-        String s2 ="       ";
+        String s2 = "       ";
         s2 = s2.trim(); //""
         boolean r1 = s2.isEmpty();
         System.out.println("r1 = " + r1);
@@ -49,23 +49,19 @@ public class String_Method {
         String str3 = new String("Cat"); // Java Heap
 
         System.out.println(str1 == str2); // false
-        System.out.println( str1.equals( str2 ) ); // true
+        System.out.println(str1.equals(str2)); // true
 
-        System.out.println( str2 == str3 ); // false
-        System.out.println( str2.equals(str3) ); //true
+        System.out.println(str2 == str3); // false
+        System.out.println(str2.equals(str3)); //true
 
         System.out.println("--------------------------------");
         String word1 = "Java";
         String word2 = "java";
 
         System.out.println(word1 == word2);// false: two different object
-        System.out.println( word1.equals(word2) ); // false: case sensitivity
+        System.out.println(word1.equals(word2)); // false: case sensitivity
 
-        System.out.println( word1.equalsIgnoreCase(word2) );
-
-
-
-
+        System.out.println(word1.equalsIgnoreCase(word2));
 
 
     }
